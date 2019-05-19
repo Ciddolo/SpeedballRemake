7871578ff453983d13295bbabf8524a3f902ddb6
Marco Iafrati <marco.iafrati@gmail.com>
2019-05-19 02:37:08 +0200

    add score and reset after goal

--- SpeedBall/Assets/Scripts/Ball/BallBehaviour.cs
+++ SpeedBall/Assets/Scripts/Ball/BallBehaviour.cs
@@ -7,16 +7,20 @@ public class BallBehaviour : MonoBehaviour
     private const float MAGNIFY_VALUE = 10.0f;
     private const float DEMAGNIFY_VALUE = 5.0f;
     private const float MAGNIFY_FORCE = 800.0f;
+    private const float MAX_SIZE = 2.5f;
+    private const float MAX_SIZE_CATCH = 1.5f;
 
     public GameObject Owner { get; set; }
 
     private BallMove ballMove;
+    private bool isCatchable;
     private Vector3 defaulSize = Vector3.one;
-    private Vector3 maxSize = new Vector3(2.0f, 2.0f, 2.0f);
+    private Vector3 maxSize = new Vector3(MAX_SIZE, MAX_SIZE, MAX_SIZE);
 
     void Start()
     {
         ballMove = GetComponent<BallMove>();
+        isCatchable = true;
     }
 
     void Update()
@@ -28,16 +32,32 @@ public class BallBehaviour : MonoBehaviour
             transform.localScale = Vector3.Lerp(transform.localScale, maxSize, MAGNIFY_VALUE * Time.deltaTime);
         else
             transform.localScale = Vector3.Lerp(transform.localScale, defaulSize, DEMAGNIFY_VALUE * Time.deltaTime);
+
+        isCatchable = transform.localScale.x <= MAX_SIZE_CATCH;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject.tag == "Goal")
+        {
+            if (other.gameObject.name == "BlueGoal")
+                GameManager.TeamScore(1);
+            else
+                GameManager.TeamScore(2);
+
+            if (Owner != null)
+                RemoveBall();
+
+            ResetPosition();
+        }
+
         if (Owner != null)
             return;
 
         if (other.gameObject.tag == "Player")
         {
-            AttachBall(other.gameObject);
+            if ((other.gameObject.name == "RED_GK" || other.gameObject.name == "BLUE_GK") || isCatchable)
+                AttachBall(other.gameObject);
         }
         else if (other.gameObject.tag == "Wall")
         {
@@ -67,5 +87,13 @@ public class BallBehaviour : MonoBehaviour
         Owner.GetComponent<PlayerManager>().Ball = null;
         Owner = null;
         transform.parent = null;
+    }
+
+    public void ResetPosition()
+    {
+        ballMove.Direction = Vector2.zero;
+        ballMove.Velocity = Vector2.zero;
+        ballMove.Force = 0.0f;
+        transform.localPosition = Vector3.zero;
     }
 }
