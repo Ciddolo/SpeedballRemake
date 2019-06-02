f51c22441085e2c5ec76be34db38fd1454a789f3
LorenzoDiStefano <lorenzodistefano.k@gmail.com>
2019-06-02 18:03:22 +0200

    Add raycast

---
+++ SpeedBallServer/SpeedBallServer.Test/ServerTests/RaycastTests.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using NUnit.Framework;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedBallServer.Test.ServerTests
+{
+    class RaycastTests
+    {
+        [Test]
+        public void RaycastObstacle()
+        {
+            Obstacle obstacle = new Obstacle(null, 5, 5);
+            Player myPlayer = new Player(null, 1, 1);
+
+            obstacle.Position = new Vector2(10, 0);
+            myPlayer.Position = new Vector2(0, 0);
+
+            PhysicsHandler physicsHandler = new PhysicsHandler();
+
+            physicsHandler.AddItem(obstacle.RigidBody);
+            physicsHandler.AddItem(myPlayer.RigidBody);
+
+            PhysicsHandler.RaycastHitInfo collider = physicsHandler.RayCast(myPlayer.Position, new Vector2(1, 0), myPlayer.RigidBody);
+            Assert.That(collider.Collider, Is.EqualTo(obstacle.RigidBody));
+            Assert.That(collider.Distance, Is.EqualTo(7.5f));
+            Assert.That(collider.Point, Is.EqualTo(new Vector2(7.5f, 0f)));
+        }
+
+        [Test]
+        public void SuccessfullRaycastObstacleWithMask()
+        {
+            Obstacle obstacle = new Obstacle(null, 5, 5);
+            Player myPlayer = new Player(null, 1, 1);
+            Player myTeammate = new Player(null, 1, 1);
+
+            obstacle.Position = new Vector2(10, 0);
+            myTeammate.Position = new Vector2(5, 0);
+            myPlayer.Position = new Vector2(0, 0);
+
+            PhysicsHandler physicsHandler = new PhysicsHandler();
+
+            physicsHandler.AddItem(obstacle.RigidBody);
+            physicsHandler.AddItem(myPlayer.RigidBody);
+            physicsHandler.AddItem(myTeammate.RigidBody);
+
+            PhysicsHandler.RaycastHitInfo collider = physicsHandler.RayCast(myPlayer.Position, new Vector2(1, 0), myPlayer.RigidBody, (uint)ColliderType.Obstacle);
+            Assert.That(collider.Collider, Is.EqualTo(obstacle.RigidBody));
+            Assert.That(collider.Distance, Is.EqualTo(7.5f));
+            Assert.That(collider.Point, Is.EqualTo(new Vector2(7.5f, 0f)));
+        }
+
+        [Test]
+        public void RaycastTeammate()
+        {
+            Obstacle obstacle = new Obstacle(null, 5, 5);
+            Player myPlayer = new Player(null, 1, 1);
+            Player myTeammate = new Player(null, 1, 1);
+
+            obstacle.Position = new Vector2(10, 0);
+            myTeammate.Position = new Vector2(5, 0);
+            myPlayer.Position = new Vector2(1, 0);
+
+            PhysicsHandler physicsHandler = new PhysicsHandler();
+
+            physicsHandler.AddItem(obstacle.RigidBody);
+            physicsHandler.AddItem(myPlayer.RigidBody);
+            physicsHandler.AddItem(myTeammate.RigidBody);
+
+            PhysicsHandler.RaycastHitInfo collider = physicsHandler.RayCast(myPlayer.Position, new Vector2(1, 0), myPlayer.RigidBody);
+            Assert.That(collider.Collider, Is.EqualTo(myTeammate.RigidBody));
+            Assert.That(collider.Distance, Is.EqualTo(3.5f));
+            Assert.That(collider.Point, Is.EqualTo(new Vector2(4.5f, 0f)));
+        }
+
+        [Test]
+        public void RaycastNothing()
+        {
+            Player myPlayer = new Player(null, 1, 1);
+
+            myPlayer.Position = new Vector2(0, 0);
+
+            PhysicsHandler physicsHandler = new PhysicsHandler();
+
+            physicsHandler.AddItem(myPlayer.RigidBody);
+
+            PhysicsHandler.RaycastHitInfo collider = physicsHandler.RayCast(myPlayer.Position, new Vector2(1, 0), myPlayer.RigidBody);
+            Assert.That(collider.Collider, Is.EqualTo(null));
+            Assert.That(collider.Distance, Is.EqualTo(float.MaxValue));
+            Assert.That(collider.Point, Is.EqualTo(new Vector2(0f, 0f)));
+        }
+
+        [Test]
+        public void RaycastNothingWithMask()
+        {
+            Obstacle obstacle = new Obstacle(null, 5, 5);
+            Player myPlayer = new Player(null, 1, 1);
+            Player myTeammate = new Player(null, 1, 1);
+
+            obstacle.Position = new Vector2(10, 0);
+            myTeammate.Position = new Vector2(5, 0);
+            myPlayer.Position = new Vector2(1, 0);
+
+            PhysicsHandler physicsHandler = new PhysicsHandler();
+
+            physicsHandler.AddItem(obstacle.RigidBody);
+            physicsHandler.AddItem(myPlayer.RigidBody);
+            physicsHandler.AddItem(myTeammate.RigidBody);
+
+            PhysicsHandler.RaycastHitInfo collider = physicsHandler.RayCast(myPlayer.Position, new Vector2(1, 0), myPlayer.RigidBody, 0);
+            Assert.That(collider.Collider, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void RaycastNothingDiagonal()
+        {
+            Obstacle obstacle = new Obstacle(null, 5, 5);
+            Player myPlayer = new Player(null, 1, 1);
+            Player myTeammate = new Player(null, 1, 1);
+
+            obstacle.Position = new Vector2(10, 0);
+            myTeammate.Position = new Vector2(5, 0);
+            myPlayer.Position = new Vector2(1, 0);
+
+            PhysicsHandler physicsHandler = new PhysicsHandler();
+
+            physicsHandler.AddItem(obstacle.RigidBody);
+            physicsHandler.AddItem(myPlayer.RigidBody);
+            physicsHandler.AddItem(myTeammate.RigidBody);
+
+            PhysicsHandler.RaycastHitInfo collider = physicsHandler.RayCast(myPlayer.Position, Vector2.Normalize(new Vector2(1, 1)), myPlayer.RigidBody);
+            Assert.That(collider.Collider, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void RaycastDiagonal()
+        {
+            Obstacle obstacle = new Obstacle(null, 5, 5);
+            Player myPlayer = new Player(null, 1, 1);
+            Player myTeammate = new Player(null, 1, 1);
+
+            obstacle.Position = new Vector2(10, 0);
+            myTeammate.Position = new Vector2(5, 0);
+            myPlayer.Position = new Vector2(1, 0);
+
+            PhysicsHandler physicsHandler = new PhysicsHandler();
+
+            physicsHandler.AddItem(obstacle.RigidBody);
+            physicsHandler.AddItem(myPlayer.RigidBody);
+            physicsHandler.AddItem(myTeammate.RigidBody);
+
+            PhysicsHandler.RaycastHitInfo collider = physicsHandler.RayCast(myPlayer.Position, Vector2.Normalize(new Vector2(1, 1)), myPlayer.RigidBody);
+            Assert.That(collider.Collider, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void RaycastDiagonalSuccessfull()
+        {
+            Obstacle obstacle = new Obstacle(null, 5, 5);
+            Player myPlayer = new Player(null, 1, 1);
+            Player myTeammate = new Player(null, 1, 1);
+
+            obstacle.Position = new Vector2(10, 1);
+            myPlayer.Position = new Vector2(5, 0);
+
+            PhysicsHandler physicsHandler = new PhysicsHandler();
+
+            physicsHandler.AddItem(obstacle.RigidBody);
+            physicsHandler.AddItem(myPlayer.RigidBody);
+            physicsHandler.AddItem(myTeammate.RigidBody);
+
+            PhysicsHandler.RaycastHitInfo collider = physicsHandler.RayCast(myPlayer.Position, Vector2.Normalize(new Vector2(1, 1)), myPlayer.RigidBody);
+            Assert.That(collider.Collider, Is.EqualTo(obstacle.RigidBody));
+            Assert.That(collider.Distance, Is.InRange(3.5f, 3.6f));
+        }
+    }
+}
--- SpeedBallServer/SpeedBallServer/Engine/PhysicsHandler.cs
+++ SpeedBallServer/SpeedBallServer/Engine/PhysicsHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,8 @@ namespace SpeedBallServer
 {
     public class PhysicsHandler
     {
-        List<RigidBody> items;
         static Collision collisionInfo;
+        List<RigidBody> items;
         List<RigidBody> itemsToRemove;
 
         public PhysicsHandler()
@@ -44,6 +45,144 @@ namespace SpeedBallServer
                 itemsToRemove.Clear();
             }
 
+        }
+
+        private float[] ConvertVector2toArray(Vector2 vec)
+        {
+            float[] res = new float[2];
+            res[0] = vec.X;
+            res[1] = vec.Y;
+            return res;
+        }
+
+        public struct RaycastHitInfo
+        {
+            public RigidBody Collider;
+            public float Distance;
+            public Vector2 Point;
+        }
+
+        const int DIMENSIONS = 2;
+        const int RIGHT = 0;
+        const int LEFT = 1;
+        const int MIDDLE = 2;
+
+        public RaycastHitInfo? RectRayIntersection(Vector2 bottomLeft, Vector2 topRight, Vector2 origin, Vector2 direction)
+        {
+            float[] bottomLeftCoords, topRightCoords, candidatePlane, maxT, originCoords, dirCoords, coordinates;
+            int[] quadrant;
+            bool inside;
+
+            bottomLeftCoords = ConvertVector2toArray(bottomLeft);
+            topRightCoords = ConvertVector2toArray(topRight);
+            originCoords = ConvertVector2toArray(origin);
+            dirCoords = ConvertVector2toArray(direction);
+
+            candidatePlane = new float[DIMENSIONS];
+            coordinates = new float[DIMENSIONS];
+            maxT = new float[DIMENSIONS];
+            quadrant = new int[DIMENSIONS];
+            inside = true;
+
+            /* Find candidate planes; this loop can be avoided if
+            rays cast all from the eye(assume perpsective view) */
+
+            for (int i = 0; i < DIMENSIONS; i++)
+            {
+                if (originCoords[i] < bottomLeftCoords[i])
+                {
+                    quadrant[i] = LEFT;
+                    candidatePlane[i] = bottomLeftCoords[i];
+                    inside = false;
+                }
+                else if (originCoords[i] > topRightCoords[i])
+                {
+                    quadrant[i] = RIGHT;
+                    candidatePlane[i] = topRightCoords[i];
+                    inside = false;
+                }
+                else
+                {
+                    quadrant[i] = MIDDLE;
+                }
+            }
+
+            // Ray origin inside bounding box 
+            if (inside)
+            {
+                return null;
+            }
+
+            // Calculate T distances to candidate planes 
+            for (int i = 0; i < DIMENSIONS; i++)
+            {
+                if (quadrant[i] != MIDDLE && dirCoords[i] != 0)
+                    maxT[i] = (candidatePlane[i] - originCoords[i]) / dirCoords[i];
+                else
+                    maxT[i] = -1f;
+            }
+
+            int whichPlane = 0;
+
+            for (int i = 1; i < DIMENSIONS; i++)
+            {
+                if (maxT[whichPlane] < maxT[i])
+                    whichPlane = i;
+            }
+
+            // Check final candidate actually inside box 
+            if (maxT[whichPlane] < 0)
+                return (null);
+
+            for (int i = 0; i < DIMENSIONS; i++)
+            {
+                if (whichPlane != i)
+                {
+
+                    coordinates[i] = originCoords[i] + maxT[whichPlane] * dirCoords[i];
+                    if (coordinates[i] < bottomLeftCoords[i] || coordinates[i] > topRightCoords[i])
+                        return (null);
+                }
+                else
+                {
+                    coordinates[i] = candidatePlane[i];
+
+                }
+            }
+
+            RaycastHitInfo raycastHit = new RaycastHitInfo();
+            raycastHit.Point = new Vector2(coordinates[0], coordinates[1]);
+            raycastHit.Distance = (origin - raycastHit.Point).Length();
+            return raycastHit;
+        }
+
+        public RaycastHitInfo RayCast(Vector2 origin, Vector2 direction, RigidBody mySelf, uint raycastMask = 31)
+        {
+            RaycastHitInfo raycastHit = new RaycastHitInfo();
+            raycastHit.Distance = float.MaxValue;
+            raycastHit.Collider = null;
+            raycastHit.Point = Vector2.Zero;
+
+            foreach (RigidBody collider in items)
+            {
+                if (collider == mySelf || (raycastMask & collider.Type) == 0)
+                    continue;
+
+                RaycastHitInfo? p = RectRayIntersection(collider.BoundingBox.BottomLeft, collider.BoundingBox.TopRight, origin, direction);
+
+                Console.WriteLine(collider.BoundingBox.BottomLeft);
+                Console.WriteLine(collider.BoundingBox.TopRight);
+
+                if (p != null)
+                {
+                    if (p.Value.Distance < raycastHit.Distance)
+                    {
+                        raycastHit = p.Value;
+                        raycastHit.Collider = collider;
+                    }
+                }
+            }
+            return raycastHit;
         }
 
         public void Update(float deltaTime)
@@ -88,6 +227,5 @@ namespace SpeedBallServer
                 }
             }
         }
-
     }
 }
